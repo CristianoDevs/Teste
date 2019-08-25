dac6d89ab4756c76dda85751a4dedaffaaeb112e
Usuario <Usuario@192.168.11.6>
2019-08-25 15:04:10 -0300

    Teste Primeiro Commit

---
+++ src/main/java/com/monte/testedebug/Principal.java
@@ -0,0 +1,16 @@
+
+package com.monte.testedebug;
+
+import com.monte.testedebug.view.FormTelaPrincipal;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Principal {
+    public static void main(String[] args) {
+        FormTelaPrincipal form = new FormTelaPrincipal();
+        form.setLocationRelativeTo(null);
+        form.setVisible(true);
+    }
+}
---
+++ src/main/java/com/monte/testedebug/validar/Mensagens.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.monte.testedebug.validar;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Mensagens {
+    
+    public String MensagemNormal(String campo){
+        JOptionPane.showMessageDialog(null, campo);
+        return null;
+    }
+    
+    public String exibirMensagemCampoVazio(String campo){
+        JOptionPane.showMessageDialog(null, "Preencha o campo " + campo, "Campo vazio", JOptionPane.INFORMATION_MESSAGE);
+        return null;
+    }
+    
+    
+    
+}
